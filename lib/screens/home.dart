7a1843937831cbba0d824d6df900d5fe0d4c3ecd
alaphy <alaphy.marcos@hotmail.com>
2022-08-24 23:04:17 -0300

    Aula 01

---
+++ lib/screens/home.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:states_widgets_routes/models/languege.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Language> languages = [
+    Language("Android Nativo", "Linguagen C"),
+    Language("...", "...")
+  ];
+
+  Widget title = const Text("Minhas Linguagens");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: title),
+      body: Column(
+        children: [
+          Wrap(
+            spacing: 10,
+            children: buildChoice(),
+          ),
+          Expanded(child: ListView(children: buildItemsList()))
+        ],
+      ),
+    );
+  }
+
+  List<Widget> buildChoice() {
+    return languages
+        .map((l) => ChoiceChip(
+            label: Text(l.title),
+            selected: l.select,
+            onSelected: (value) => setState(() {
+                  l.select = value;
+                })))
+        .toList();
+  }
+
+  List<Widget> buildItemsList() {
+    return languages
+        .where((l) => l.select)
+        .map((l) => Card(
+              child: ListTile(
+                leading: Icon(l.icon),
+                title: Text(l.title),
+                subtitle: Text(l.subTitle),
+              ),
+            ))
+        .toList();
+  }
+}
